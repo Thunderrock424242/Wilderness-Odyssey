530d52687793ec4ea1230d9531fd7cc6ebcafb81
Thunderrock424242 <masonjoshua8@gmail.com>
2024-05-21 20:21:56 -0400

    fixed annoying weather

--- kubejs/server_scripts/serene seasons.js
+++ kubejs/server_scripts/serene seasons.js
@@ -7,5 +7,5 @@ console.info('Hello, World! (Loaded server scripts)')
 ServerEvents.loaded(e => {
     if (e.server.persistentData.firstLoad) return
     e.server.persistentData.firstLoad = true
-    e.server.runCommandSilent('season set LATE_WINTER')
+    e.server.runCommandSilent('season set EARLY_SPRING')
   })
