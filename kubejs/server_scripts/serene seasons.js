8f8e90f8883331a93b21fd13af9fbc8e80c02756
Thunderrock424242 <masonjoshua8@gmail.com>
2024-05-21 20:20:24 -0400

    added kubejs

---
+++ kubejs/server_scripts/serene seasons.js
@@ -0,0 +1,11 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! (Loaded server scripts)')
+
+ServerEvents.loaded(e => {
+    if (e.server.persistentData.firstLoad) return
+    e.server.persistentData.firstLoad = true
+    e.server.runCommandSilent('season set LATE_WINTER')
+  })
